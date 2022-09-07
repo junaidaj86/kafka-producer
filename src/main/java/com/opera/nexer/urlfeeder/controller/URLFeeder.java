de7f3a22b5bd827cc8cf2b8a5e326cab35aaa047
Junaid Ahmed <junaid.ahmed@nexergroup.com>
2022-09-07 16:27:54 +0530

    making it generic and adding logs

--- src/main/java/com/opera/nexer/urlfeeder/controller/URLFeeder.java
+++ src/main/java/com/opera/nexer/urlfeeder/controller/URLFeeder.java
@@ -3,6 +3,8 @@ package com.opera.nexer.urlfeeder.controller;
 import com.opera.nexer.urlfeeder.common.Constants;
 import com.opera.nexer.urlfeeder.model.URL;
 import com.opera.nexer.urlfeeder.service.URLService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,8 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1")
 public class URLFeeder {
+
+    Logger LOGGER = LoggerFactory.getLogger(URLFeeder.class);
 
     @Autowired
     private URLService urlService;
@@ -26,6 +30,7 @@ public class URLFeeder {
     public ResponseEntity<Void> submitUrl(@RequestBody URL url){
         url.setId(Constants.URL_UUID_PREFIX +UUID.randomUUID().toString());
         url.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+        LOGGER.info("URL received" + url);
         urlService.save(url);
         return ResponseEntity.ok().build();
     }
